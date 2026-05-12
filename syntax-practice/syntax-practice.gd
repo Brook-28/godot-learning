8cca7cc24d413685016d95bf94cb52aa61945ad7
Brook-28 <rajanas28n@gmail.com>
2026-05-12 17:15:46 +0300

    finished all the syntax exercises

--- syntax-practice/syntax-practice.gd
+++ syntax-practice/syntax-practice.gd
@@ -1,5 +1,11 @@
 extends Node
 
+var cost = 0
+func total_cost(cart, dict):
+		for item in cart:
+			cost += dict[item]
+		print(cost)
+		
 func _ready() -> void:
 	
 	# 1.1 Variables and types
@@ -52,8 +58,33 @@ func _ready() -> void:
 			print("Buzz")
 		else:
 			print(number)
+		
+	# 4.1 Arrays
+	var fruits = ["banana", "orange", "apple", "mango", "lemon"]
+	print(fruits[0])
+	print(fruits[-1])
+	print(fruits.size())
 
-
-
-
+	# 4.2
+	var numbers = [4, 7, 2, 9, 1]
+	var biggest_number = 0
+	for biggest in numbers:
+		if biggest > biggest_number:
+			biggest_number = biggest
+	print(biggest_number)
 	
+	# 4.3
+	var squared_numbers = []
+	for number in range(1, 9):
+		squared_numbers.append(number ** 2)
+	print(squared_numbers)
+	
+	# 5.1 Dictionaries
+	var person = {"name": "Walter", "age": "50", "city": "Albuquerque"}
+	for pair in person:
+		print(pair, " ", person[pair])
+
+	# 5.2 
+	var product = {"Chips": 2.54, "Cola": 2.09, "Burger": 4.23}
+	
+	total_cost(["Chips", "Cola", "Burger"], product)
