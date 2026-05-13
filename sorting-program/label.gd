39115c3db04523d157f6b6cf2c98c3804ddfcdaa
Brook-28 <rajanas28n@gmail.com>
2026-05-13 21:04:12 +0300

    made a bubble sort program

---
+++ sorting-program/label.gd
@@ -0,0 +1,42 @@
+extends Label
+
+func array_sort(numbers) -> Array:
+	
+	var swapped = true
+	
+	while swapped == true:
+		swapped = false
+		for number in range(numbers.size() - 1):
+			
+			var number1position = number
+			var number2position = number + 1
+		
+		
+			var number1 = numbers[number1position]
+			var number2 = numbers[number2position]
+		
+			if number1 > number2:
+				swapped = true
+				numbers.remove_at(number1position)
+				numbers.insert(number2position, number1)
+				
+		
+				print(numbers)
+				
+	return numbers
+
+var time_passed = 0.0
+
+func _process(delta: float) -> void:
+	time_passed += delta
+	
+	if time_passed >= 6.0:
+		time_passed = 0.0
+		var numbers = []
+		for i in range(20):
+			numbers.append(randi() % 100)
+			
+		var result = array_sort(numbers.duplicate())
+		print("The sorted list is ", result)
+		
+		text = "Random: " + str(numbers) + "\nSorted: " + str(result)
